6fe16996750e30db2db7cc8c568a7a2d22c19ee3
edelvarden <edelvarden@gmail.com>
2023-08-08 16:48:06 +0300

    added check icon

--- public/content.js
+++ public/content.js
@@ -9,40 +9,102 @@
     callback()
   }
 
-  const CopyButton = (favoriteButtonClassList) => {
+  /**
+   * Creates a copy button element.
+   * @param {Array} favoriteButtonClassList - List of classes for the button.
+   * @returns {HTMLElement} - The copy button element.
+   */
+  const createCopyButton = (favoriteButtonClassList) => {
     const copyTrackNameButton = document.createElement("button")
-    copyTrackNameButton.innerHTML = `<span style="display: flex;"><svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor" ><path d="M216-96q-29.7 0-50.85-21.15Q144-138.3 144-168v-552h72v552h456v72H216Zm144-144q-29.7 0-50.85-21.15Q288-282.3 288-312v-480q0-29.7 21.15-50.85Q330.3-864 360-864h384q29.7 0 50.85 21.15Q816-821.7 816-792v480q0 29.7-21.15 50.85Q773.7-240 744-240H360Zm0-72h384v-480H360v480Zm0 0v-480 480Z"/></svg></span>`
-
     copyTrackNameButton.classList.add("copy-button")
     if (favoriteButtonClassList) {
       copyTrackNameButton.classList.add(...favoriteButtonClassList)
     }
-
     copyTrackNameButton.setAttribute(
       "aria-label",
       "Copy track name to clipboard"
     )
 
-    copyTrackNameButton.addEventListener("click", async () => {
-      const artistTitle = document.querySelector(
-        "div[data-testid='context-item-info-subtitles']"
-      )
-      const trackName = document.querySelector(
-        "div[data-testid='now-playing-widget'] div[title]"
-      )
+    const copyIcon = createCopyIcon()
+    const checkIcon = createCheckIcon()
+    checkIcon.style.display = "none"
 
-      if (artistTitle && trackName) {
-        const artistText = artistTitle.textContent
-        const trackText = trackName.getAttribute("title")
-        const copyText = `${artistText} - ${trackText}`
+    copyTrackNameButton.appendChild(copyIcon)
+    copyTrackNameButton.appendChild(checkIcon)
 
-        await navigator.clipboard.writeText(copyText)
-      }
+    copyTrackNameButton.addEventListener("click", () => {
+      copyToClipboard()
+      showCheckIcon(checkIcon, copyIcon)
     })
 
-    return copyTrackNameButton;
+    return copyTrackNameButton
   }
 
+  // Create the SVG icon for the button
+  const createCopyIcon = () => {
+    const copyIcon = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    )
+    copyIcon.setAttribute("height", "20")
+    copyIcon.setAttribute("viewBox", "0 -960 960 960")
+    copyIcon.setAttribute("width", "20")
+    copyIcon.setAttribute("fill", "currentColor")
+    copyIcon.innerHTML = `
+    <path d="M216-96q-29.7 0-50.85-21.15Q144-138.3 144-168v-552h72v552h456v72H216Zm144-144q-29.7 0-50.85-21.15Q288-282.3 288-312v-480q0-29.7 21.15-50.85Q330.3-864 360-864h384q29.7 0 50.85 21.15Q816-821.7 816-792v480q0 29.7-21.15 50.85Q773.7-240 744-240H360Zm0-72h384v-480H360v480Zm0 0v-480 480Z"/>
+  `
+    return copyIcon
+  }
+
+  // Create the check SVG icon
+  const createCheckIcon = () => {
+    const checkIcon = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    )
+    checkIcon.setAttribute("height", "24")
+    checkIcon.setAttribute("viewBox", "0 -960 960 960")
+    checkIcon.setAttribute("width", "24")
+    checkIcon.setAttribute("fill", "#1ed760")
+    checkIcon.innerHTML = `
+    <path d="M395-285 226-455l50-50 119 118 289-288 50 51-339 339Z"/>
+  `
+    return checkIcon
+  }
+
+  /**
+   * Copies track information to the clipboard.
+   */
+  const copyToClipboard = async () => {
+    const artistTitle = document.querySelector(
+      "div[data-testid='context-item-info-subtitles']"
+    )
+    const trackName = document.querySelector(
+      "div[data-testid='now-playing-widget'] div[title]"
+    )
+
+    if (artistTitle && trackName) {
+      const artistText = artistTitle.textContent
+      const trackText = trackName.getAttribute("title")
+      const copyText = `${artistText} - ${trackText}`
+
+      await navigator.clipboard.writeText(copyText)
+    }
+  }
+
+  const showCheckIcon = (checkIcon, svgIcon) => {
+    checkIcon.style.display = "inline"
+    svgIcon.style.display = "none"
+
+    afterDelay(() => {
+      checkIcon.style.display = "none"
+      svgIcon.style.display = "inline"
+    }, 2000)
+  }
+
+  /**
+   * Waits for required elements to load and adds the copy button.
+   */
   const waitForElements = async () => {
     const favoriteButton = document.querySelector("[data-testid='add-button']")
     const favoriteButtonClassList = favoriteButton
@@ -54,12 +116,12 @@
     )
 
     if (nowPlayingWidget) {
-      nowPlayingWidget.appendChild(CopyButton(favoriteButtonClassList))
+      nowPlayingWidget.appendChild(createCopyButton(favoriteButtonClassList))
     } else {
       afterDelay(waitForElements, 1000)
     }
   }
 
+  // Entry point
   window.addEventListener("load", waitForElements)
-
 })()
